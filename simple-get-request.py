4cd5ed6a8a4afe89a6b21bb8d433d375f70a4acc
Your Name <your.email@example.com>
2026-03-02 23:16:33 -0500

    updated ci with test file

--- simple-get-request.py
+++ simple-get-request.py
@@ -4,11 +4,7 @@ BASE_URL = "https://jsonplaceholder.typicode.com"
 
 def test_get_post():
     response = requests.get(f"{BASE_URL}/posts/1")
-    
     assert response.status_code == 200
-    
+
     data = response.json()
-    print(data)
     assert data["userId"] == 1
-    assert "title" in data
-    assert data["id"] == 1
